a5ef9cc4b76a4ce44b89c61619217b02559cb12d
aaryakhedekar <khedekaraarya36@gmail.com>
2025-07-01 17:16:40 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -1,37 +1,12 @@
-<<<<<<< HEAD
-const App = () =>{
-
-
-  return (<>
-  
-  <h1> Hello</h1>
-  </>)
-}
-export default App;
-=======
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world!</h1>
     </div>
   );
 }
 
 export default App;
->>>>>>> a46217d37dfc03f3f5112fee6bd8367d9f192d61
