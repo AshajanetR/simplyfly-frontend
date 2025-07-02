34a6677b31779d468a9400f7c0f0141d726768e9
aaryakhedekar <khedekaraarya36@gmail.com>
2025-07-02 17:02:36 +0530

    merged

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -3,12 +3,13 @@ import SignUp from '../Component/SignUp';
 import SignIn from '../Component/SignIn';
 import Dealscard from '../Component/Dealscard';
 import DCardApi from './DCardApi';
+import FlightSearchBar from '../Components/FlightSearchBar/FlightSearchBar';
 
 
 const Landing = () => {
 
   return (
-    <div>Landing</div>
+    <FlightSearchBar/>
   )
 }
 
