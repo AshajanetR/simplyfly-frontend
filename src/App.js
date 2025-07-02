f70600a1204ab1fee3a8a30234d17cbed7af007f
Sai Jog <saijog66@gmail.com>
2025-07-02 17:57:15 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Landing from "./pages/Landing";
 import HeaderBefore from "./components/HeaderBefore";
 import HeaderAfter from "./components/HeaderAfter";
 import Profile from "./components/Profile";
+import FlightSearchBar from '../components/FlightSearchBar/FlightSearchBar';
+
 
 const App = () => {
   return (
