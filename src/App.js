92abb31358265577dd19fa1bbe5a5ecd95cc90bd
Sai Jog <saijog66@gmail.com>
2025-07-14 09:15:40 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Routing from "./Routes/Routing"
+import Landing from "./pages/Landing/Landing";
 const App = () => {
   return (
     <div>
       {/* <Landing /> */}
       <Routing />
+      <Landing />
     </div>
   )
 }
